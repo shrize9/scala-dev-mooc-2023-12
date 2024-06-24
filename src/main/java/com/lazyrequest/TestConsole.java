eff31bddac2ba5377324178092ba4e1f094798c0
p_kuzmin <pavel.kuzmin@indriver.com>
2024-06-24 19:59:01 +0300

    Добавлен Console dsl

--- src/main/java/com/lazyrequest/TestConsole.java
+++ src/main/java/com/lazyrequest/TestConsole.java
@@ -5,6 +5,12 @@ import org.graalvm.collections.Pair;
 
 public class TestConsole {
     public static void main(String[] args) {
+        //enter name:
+        //t =pavel
+        //enter age:
+        //t1 =10
+        //you entered (PAVEL, 10)
+
         Console<String> readName =((Console<String>)(new Console().println("enter name:").readLine())).map((t)->t.toUpperCase());
         Console<Integer> readAge = ((Console<String>)(new Console().println("enter age:").readLine())).map((t)->Integer.decode(t));
 
